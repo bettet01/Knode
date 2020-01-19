1aa1e898f85edf1c317c0456acef1d8ac1f58e55
bettet01 <bettet01@luther.edu>
2020-01-19 00:00:23 -0600

    added firebase init

--- knode-v0.1/src/App.js
+++ knode-v0.1/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.png'
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <div>
-      Test
+    <div className='test'>
+      <Particles />
+      <img className='logo' src={logo} alt="logo" />
     </div>
   );
 }
