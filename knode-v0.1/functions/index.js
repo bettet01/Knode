96dd4fd44ca41fd00596c0fd1a232a8c01466a1e
bettet01 <bettet01@luther.edu>
2020-01-21 18:38:53 -0600

    removed bogus code

--- knode-v0.1/functions/index.js
+++ knode-v0.1/functions/index.js
@@ -9,24 +9,3 @@ admin.initializeApp();
 const db = admin.firestore();
 
 
-exports.postOneScream = (req, res) => {
-    if(req.body.body.trim() === '') {
-        return res.status(400).json({ body: 'Body must not be empty'})
-    }
-    const newScream = {
-        body: req.body.body,
-        userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
-    }
-
-    db
-        .collection('screams')
-        .add(newScream)
-        .then((doc) => {
-            return res.json({ message: `document ${doc.id} created succussfully` });
-        })
-        .catch(err => {
-            res.status(500).json({ error: 'something went wrong'});
-            console.log(err);
-        })
-}
