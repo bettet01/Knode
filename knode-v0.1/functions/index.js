0b60baf04b614356a02dcf118bba0118ac3e00f9
bettet01 <bettet01@luther.edu>
2020-01-21 18:35:28 -0600

    added something

--- knode-v0.1/functions/index.js
+++ knode-v0.1/functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const express = require('express')
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// init express and add to app
+const app = express();
+admin.initializeApp();
+const db = admin.firestore();
+
+
+exports.postOneScream = (req, res) => {
+    if(req.body.body.trim() === '') {
+        return res.status(400).json({ body: 'Body must not be empty'})
+    }
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.user.handle,
+        createdAt: new Date().toISOString()
+    }
+
+    db
+        .collection('screams')
+        .add(newScream)
+        .then((doc) => {
+            return res.json({ message: `document ${doc.id} created succussfully` });
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong'});
+            console.log(err);
+        })
+}
