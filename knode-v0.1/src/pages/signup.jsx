4c2015feb33da3f24aecdeb0859c65d36c36d677
bettet01 <bettet01@luther.edu>
2020-02-06 14:42:57 -0600

    hotfix for a positioning error on the signup page

--- knode-v0.1/src/pages/signup.jsx
+++ knode-v0.1/src/pages/signup.jsx
@@ -56,7 +56,7 @@ const handleSubmit = (event) => {
   return(
     <Grid container spacing={3}>
       <Grid item md>
-        <Paper elevation={3} style={{ textAlign: "center", marginTop: "20%"}}>
+        <Paper elevation={3} style={{ textAlign: "center", marginTop: "20%", marginLeft: "30px", marginRight: "30px"}}>
         <img src={AppIcon} alt="logo" style={{ height: 250 }}/>
         <Typography varient="main" style={{ padding: 30, width: "75%", marginLeft: "auto", marginRight: "auto"}}>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
